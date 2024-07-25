70ade8c5fbe2591ccb8a70d1bbbe77773fdc0a4e
sunilyadavfabhotel <sunil.yadav@fabhotels.com>
2024-07-25 13:40:20 +0530

    update: added few basic components

--- App.tsx
+++ App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import {Container, ListComponent} from './src/components';
+import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
+import {
+  AlertModal,
+  AutoSuggestComponent,
+  Container,
+  DropdownComponent,
+  InputComponent,
+  ListComponent,
+} from './src/components';
 import {Text} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <StatusBar barStyle="dark-content" />
+      <ScrollView>
+        <StatusBar barStyle="dark-content" />
 
-      {/* Text component */}
-      <Container>
-        <Text className="text-24 text-red-300">
-          React Native Web App Example
-        </Text>
-      </Container>
+        {/* Text component */}
+        <Container title="Text">
+          <Text className="text-24 text-red-300">
+            React Native Web App Example
+          </Text>
+        </Container>
 
-      {/* List */}
-      <Container>
-        <ListComponent />
-      </Container>
+        {/* List */}
+        <Container title="List">
+          <ListComponent />
+        </Container>
+
+        {/* Input field */}
+        <Container title="Enter some text">
+          <InputComponent />
+        </Container>
+
+        {/* Dropdown */}
+        <Container title="Dropdown">
+          <DropdownComponent />
+        </Container>
+
+        {/* Auto suggest */}
+        <Container title="Auto Suggest">
+          <AutoSuggestComponent />
+        </Container>
+
+        {/* Alert Modal */}
+        <Container title="Alert Modal">
+          <AlertModal />
+        </Container>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,15 @@
 import {Container} from './Container';
+import {AlertModal} from './AlertModal';
 import {ListComponent} from './List';
+import {InputComponent} from './InputField';
+import {DropdownComponent} from './DropdownComponent';
+import {AutoSuggestComponent} from './AutoSuggestComponent';
 
-export {Container, ListComponent};
+export {
+  Container,
+  AlertModal,
+  ListComponent,
+  InputComponent,
+  DropdownComponent,
+  AutoSuggestComponent,
+};
